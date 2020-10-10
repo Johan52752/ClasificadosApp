4d9d20cd8f180091e79c3fb8aa92a7259b45b00e
Johitan <johan52754@gmail.com>
2020-10-10 08:40:01 -0500

    Subiendo archivos de la aplicacion

--- Archivo.js
+++ Archivo.js
@@ -1,19 +1,44 @@
-const nv = 3;
+let carr = {
+    galones: 0,
+    maxgalones: 15,
+    kmRecorridos: 0,
+    tanquear: function(galones) {
+        if (galones < 0 || galones > this.maxgalones) {
+            console.log("No se puede tanquear el carro");
+        } else {
+            if (this.galones <= this.maxgalones) {
+                this.galones += galones;
+                console.log("El carro se tanqueo correctamente con " + galones + " Galones")
+            } else {
+                console.log("No se pudo tanquear el carro")
+            }
+        }
+    },
+    mover: function(Kilometros) {
+        if (Kilometros > 0 || Kilometros < this.galones) {
+            this.kmRecorridos += Kilometros;
+            console.log("Se movio el carro " + Kilometros + " Kilometros")
+        } else {
+            console.log("No se movio el carro");
+        }
+    },
 
-for (let i = 1; i <= nv; i++) {
-    const val = 120000 * i;
-    console.log(proceso(val));
+    getKilometros: function() {
+        console.log(this.kmRecorridos);
+    }
+
 }
 
-
-function proceso(n) {
-    const h = n;
+function process(n) {
     if (n >= 120000 && n < 200000) {
-        n = h - (n * 0.10);
+        console.log("Se hizo un descuento del 10% a: ", n);
+        n -= (n * 0.10);
     } else if (n >= 200000 && n <= 500000) {
-        n = h - (n * 0.08);
+        console.log("Se hizo un descuento del 8% a: ", n);
+        n -= (n * 0.08);
     } else if (n > 500000) {
-        n = h - (n * 0.05);
+        console.log("Se hizo un descuento del 5% a: ", n);
+        n -= (n * 0.05);
     }
     return (n);
 }
